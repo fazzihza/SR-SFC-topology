a2614057b2cc9183d5322000bc6a6a296d3bdc0a
fazzihza <fazzaihza@gmail.com>
2025-01-06 12:55:14 +0700

    PC Interfaces

--- PC-interfaces.sh
+++ PC-interfaces.sh
@@ -15,5 +15,5 @@ sudo docker exec -d clab-srsfc-router1 ip -6 route add fc00:1::bb/128 encap seg6
 #sudo docker exec -d clab-srsfc-router3 ip netns exec vnf2 sysctl -w net.ipv6.conf.all.seg6_enabled=1
 #sudo docker exec -d clab-srsfc-router3 ip netns exec vnf1 ip -6 route add fc00:31::bb/128 encap seg6local action End dev veth0-vnf1
 #sudo docker exec -d clab-srsfc-router3 ip netns exec vnf2 ip -6 route add fc00:32::bb/128 encap seg6local action End dev veth0-vnf2
-#sudo docker exec -d clab-srsfc-router4 ip -6 route add fc00:4::bb/128 encap seg6local action End.DX4 nh4 10.0.2.1 dev eth1
-#sudo docker exec -d clab-srsfc-router4 ip route add 10.0.0.0/24 encap seg6 mode encap segs fc00:32::bb,fc00:31::bb,fc00:1::bb dev eth1
+sudo docker exec -d clab-srsfc-router4 ip -6 route add fc00:4::bb/128 encap seg6local action End.DX4 nh4 10.0.2.1 dev eth1
+sudo docker exec -d clab-srsfc-router4 ip route add 10.0.0.0/24 encap seg6 mode encap segs fc00:32::bb,fc00:31::bb,fc00:1::bb dev eth1
